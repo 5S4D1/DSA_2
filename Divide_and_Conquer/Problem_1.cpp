f95efc6125416b6f0749eb5d486ff2348afd0606
Sadi <123616359+Sadi86@users.noreply.github.com>
2024-07-02 01:56:45 +0600

    divide and conquer

---
+++ Divide_and_Conquer/Problem_1.cpp
@@ -0,0 +1,35 @@
+/*
+    Problem-1: Write a function print_odd using divide-and-conquer algorithm to
+        print the odd numbers of an array of n integers.
+*/
+#include <iostream>
+using namespace std;
+
+int countOdd(int array[], int start, int end)
+{
+    if (start == end)
+    {
+        if (array[start] % 2 != 0)
+            return 1;
+        else
+            return 0;
+    }
+    else
+    {
+        int mid = (start + end) / 2;
+        int first_half = countOdd(array, start, mid);
+        int second_half = countOdd(array, mid + 1, end);
+
+        return first_half + second_half;
+    }
+}
+
+int main()
+{
+    int Array[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int length = sizeof(Array) / sizeof(int);
+
+    cout << "Odd Number in this array : " << countOdd(Array, 0, length - 1) << endl;
+
+    return 0;
+}
